1dac392e96aaeee69998ae91f2ea97b6fa6e21e0
sjndowair <wodnjsdl1125@naver.com>
2025-04-14 15:26:02 +0900

    라우트 기능 추가 완료

--- components/App.js
+++ components/App.js
@@ -4,7 +4,7 @@ import { request } from "./api.js";
 
 export default function App($app) {
   this.state = {
-    curruntTab: "all",
+    curruntTab: window.location.pathname.replace("/", "") || "all",
     photos: [],
   };
 
@@ -12,11 +12,8 @@ export default function App($app) {
     $app,
     initialState: "all",
     onClick: async (value) => {
-      this.setState({
-        ...this.state,
-        curruntTab: value,
-        photos: await request(value === "all" ? "" : value),
-      });
+      history.pushState(null, `${value} 사진`, `${value}`);
+      this.updateContent(value === "all" ? "" : value);
     },
   });
   const content = new Content({
@@ -30,16 +27,26 @@ export default function App($app) {
     content.setState(this.state.photos);
   };
 
-  const init = async () => {
-    const initialPhotos = await request();
+  this.updateContent = async (tabName) => {
+    const currentTab = tabName === "all" ? "" : tabName;
+    const initialPhotos = await request(currentTab);
     try {
       this.setState({
         ...this.state,
+        curruntTab: tabName,
         photos: initialPhotos,
       });
     } catch (err) {
       console.log(err);
     }
+  };
+
+  window.addEventListener("popstate", async () => {
+    this.updateContent(window.location.pathname.replace("/", "") || "all");
+  });
+
+  const init = async () => {
+    this.updateContent(this.state.curruntTab);
   };
   init();
 }
